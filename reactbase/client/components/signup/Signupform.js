4e9872bf8872a2288ebf31a3b82445db9801130b
krishk6 <Kumar.Krishnamurthy@emc.com>
2017-05-22 01:55:06 +0530

    add server side validation

--- reactbase/client/components/signup/Signupform.js
+++ reactbase/client/components/signup/Signupform.js
@@ -2,6 +2,7 @@ import React from 'react';
 import timezones from '../data/timezones';
 import map from 'lodash/map';
 import axios from 'axios'
+import classnames from 'classnames';
 
 
 class Signupform extends React.Component {
@@ -12,7 +13,9 @@ class Signupform extends React.Component {
       email:'',
       password:'',
       passwordConfirmation:'',
-      timezone:''
+      timezone:'',
+      errors:{},
+      isLoading: false
     }
 
     this.onChange = this.onChange.bind(this);
@@ -24,21 +27,26 @@ onChange(e){
 }
 
 onSubmit(e){
+  this.setState({errors:{},isLoading:true});
   e.preventDefault();
   console.log(this.state);
   //axios.post('/api/users',{user:this.state});
-  this.props.userSignupRequest(this.state);
+  this.props.userSignupRequest(this.state).then(
+    () =>{},
+    ({data}) => {this.setState({errors:data,isLoading:false})}
+  );
 }
 
 
   render(){
+    const {errors} = this.state;
     const options = map(timezones,(val,key)=>
     <option key={val} value={val}>{key}</option>
   )
     return(
       <form onSubmit={this.onSubmit}>
         <h1>Join our community!</h1>
-          <div className="form-group">
+          <div className={classnames("form-group",{'has-error': errors.username})}>
             <label className="control-label">UserName</label>
             <input
               value={this.state.username}
@@ -47,9 +55,10 @@ onSubmit(e){
               name="username"
               className="form-control"
               />
+            {errors.username && <span className="help-block">{errors.username}</span>}
           </div>
 
-          <div className="form-group">
+          <div className={classnames("form-group",{'has-error': errors.email})}>
             <label className="control-label">Email</label>
             <input
               value={this.state.email}
@@ -58,9 +67,10 @@ onSubmit(e){
               name="email"
               className="form-control"
               />
+            {errors.email && <span className="help-block">{errors.email}</span>}
           </div>
 
-          <div className="form-group">
+          <div className={classnames("form-group",{'has-error': errors.password})}>
             <label className="control-label">Password</label>
             <input
               value={this.state.password}
@@ -69,9 +79,10 @@ onSubmit(e){
               name="password"
               className="form-control"
               />
+            {errors.password && <span className="help-block">{errors.password}</span>}
           </div>
 
-          <div className="form-group">
+          <div className={classnames("form-group",{'has-error': errors.passwordConfirmation})}>
             <label className="control-label">Password Confirmation</label>
             <input
               value={this.state.passwordConfirmation}
@@ -80,9 +91,10 @@ onSubmit(e){
               name="passwordConfirmation"
               className="form-control"
               />
+            {errors.passwordConfirmation && <span className="help-block">{errors.passwordConfirmation}</span>}
           </div>
 
-          <div className="form-group">
+          <div className={classnames("form-group",{'has-error': errors.timezone})}>
             <label className="control-label">Timezone</label>
             <select
               value={this.state.timezone}
@@ -94,13 +106,14 @@ onSubmit(e){
               <option value="" disabled>Choose Your timezone</option>
               {options}
             </select>
+            {errors.timezone && <span className="help-block">{errors.timezone}</span>}
           </div>
 
 
 
 
           <div className="form-group">
-            <button className="btn btn-primary btn-lg">
+            <button disabled={this.state.isLoading} className="btn btn-primary btn-lg">
             Sign up
             </button>
           </div>
