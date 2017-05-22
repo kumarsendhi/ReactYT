22af655b661f8bb10cc267284bc39da2010900db
krishk6 <Kumar.Krishnamurthy@emc.com>
2017-05-22 16:00:58 +0530

    Saves users in database

--- reactbase/server/routes/users.js
+++ reactbase/server/routes/users.js
@@ -1,6 +1,9 @@
 import express from 'express';
 
 import validateInput from '../shared/validations/signup';
+import bcrypt from 'bcrypt';
+
+import User from '../models/user';
 
 let router = express.Router();
 
@@ -9,10 +12,21 @@ router.post('/',(req,res)=>{
 
   console.log(req.body);
   const {errors,isValid} = validateInput(req.body);
-  if(!isValid){
-    res.status(400).json(errors);
-  }else{
+  if(isValid){
+    console.log("invalid entered");
+    const { username, password, timezone, email} = req.body;
+    const password_digest = bcrypt.hashSync(password,10)
+    console.log(password_digest);
+
+    User.forge({
+      username,timezone,email,password_digest
+    }, {hasTimestamps:true}).save() .then(user => res.json({success:true}))
+    .catch(err => res.status(500).json({error:err}));
+
     res.json({success:true})
+  }else{
+
+    res.status(400).json(errors);
   }
 });
 
